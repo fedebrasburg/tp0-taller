d1711f1abf0457c31e6669a88f0bd95e2476d495
fedebrasburg <federico.brasburg@gmail.com>
2017-08-17 00:23:13 -0300

    fix in weather service

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  WeatherService().getWeather('fede').then(weather => response.json(weather));
+app.get('/weather/:cityId', function(request, response) {
+  WeatherService().getWeather(request.params.cityId).then(weather => response.json(weather));
 });
 
 app.listen(app.get('port'), function() {
--- weather/services/weatherService.js
+++ weather/services/weatherService.js
@@ -1,13 +1,19 @@
 var fetch = require('node-fetch');
+// 6559994 es la de Buenos Aires ;)
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?id=6559994&APPID=589099769890849ac40bc3c154da93a8';
 
 module.exports = function weatherService() {
 
     var self = {
         getWeather: (cityId) => {
-            return fetch(url).then((weather) => weather.json());
+            return fetch(makeUrl(cityId)).then((weather) => weather.json());
         }
     };
+
     return self;
 };
+
+const makeUrl = (cityId) => {
+    console.log( 'http://api.openweathermap.org/data/2.5/weather?id='+cityId+'&APPID=589099769890849ac40bc3c154da93a8');
+    return 'http://api.openweathermap.org/data/2.5/weather?id='+cityId+'&APPID=589099769890849ac40bc3c154da93a8';
+}
