09cb2e3a2769b9aeccba537dcb9884ab1d0814be
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-08-25 18:57:23 -0300

    Use chai for tests

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
-const assert = require('assert')
 const WeatherService = require('./weather/services/weatherService.js')
+const chai = require('chai')
+const expect = chai.expect
 
 describe('WeatherService', () => {
     describe('#getCities()', () => {
         it('should return empty array when sending a name with only one letter', () => {
             return WeatherService().getCities("b").then((data) => {
-                assert.equal(0, data.length)
+                expect(data.length).to.equal(0)
             })
         })
         it('should return empty array when sending a name that does not have a matching file', () => {
             return WeatherService().getCities("xz").then((data) => {
-                assert.equal(0, data.length)
+                expect(data.length).to.equal(0)
             })
         })
         it('should return a non empty array when sending a name with two letters', () => {
             return WeatherService().getCities("bu").then((data) => {
-                assert.notEqual(0, data.length)
+                expect(data.length).to.be.greaterThan(0)
+            })
+        })
+        it('should return a non empty array when sending a name with more than two letters', () => {
+            return WeatherService().getCities("buenos").then((data) => {
+                expect(data.length).to.be.greaterThan(0)
             })
         })
     })
